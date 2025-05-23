d29bbf1c189565dfe4ec9166a08c6bfac7327a3c
IPASAPP <shsgroupe2021@gmail.com>
2025-05-23 20:41:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function isInStandaloneMode() {
-  return (window.matchMedia('(display-mode: standalone)').matches) || (window.navigator.standalone === true);
+function isStandalone() {
+  return window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true;
 }
 
 window.onload = () => {
@@ -7,14 +7,23 @@ window.onload = () => {
   const catalog = document.getElementById('catalog');
   const closeBtn = document.getElementById('close-btn');
 
-  if (!isInStandaloneMode()) {
-    modal.style.display = 'flex';
-  } else {
+  if (isStandalone()) {
+    // Устройство запущено с главного экрана
+    modal.style.display = 'none';
     catalog.style.display = 'block';
+  } else {
+    // Проверим, не закрывал ли пользователь раньше окно
+    const wasDismissed = localStorage.getItem('a2hs-dismissed');
+    if (!wasDismissed) {
+      modal.style.display = 'flex';
+    } else {
+      catalog.style.display = 'block';
+    }
   }
 
   closeBtn.onclick = () => {
     modal.style.display = 'none';
     catalog.style.display = 'block';
+    localStorage.setItem('a2hs-dismissed', 'true'); // не показывать снова
   };
 };
