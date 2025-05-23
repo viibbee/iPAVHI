bffe1a72c6dec7b2eaf1a5eaa25aaf44af805dbd
IPASAPP <shsgroupe2021@gmail.com>
2025-05-23 23:23:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const isStandalone = window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true;
   const installModal = document.getElementById("install-modal");
+  const appInterface = document.getElementById("app-interface");
 
   if (!isStandalone) {
     installModal.classList.remove("hidden");
+  } else {
+    appInterface.classList.remove("hidden");
+    document.body.style.overflow = "auto";
   }
 });
