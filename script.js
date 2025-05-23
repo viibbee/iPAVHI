783c9286804699bd1f76caf54c24e42c243a7378
IPASAPP <shsgroupe2021@gmail.com>
2025-05-24 01:47:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const isStandalone = window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true;
 
-  console.log("window.navigator.standalone =", window.navigator.standalone);
-  console.log("window.matchMedia('(display-mode: standalone)').matches =", window.matchMedia('(display-mode: standalone)').matches);
-  console.log("Detected standalone mode:", isStandalone);
-
-  if (!isStandalone) {
-    // Показываем инструкцию по установке, прячем нижнюю панель
-    installModal.classList.remove("hidden");
-    if (bottomNav) bottomNav.classList.add("hidden");
-  } else {
-    // Скрываем инструкцию, показываем нижнюю панель
+  if (isStandalone) {
+    document.body.classList.add("standalone");
     installModal.classList.add("hidden");
     if (bottomNav) bottomNav.classList.remove("hidden");
+  } else {
+    installModal.classList.remove("hidden");
+    if (bottomNav) bottomNav.classList.add("hidden");
   }
 });
